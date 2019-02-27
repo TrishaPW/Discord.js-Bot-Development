ac8665acf4c3486c7ca9abbaf6eae28fe15d704c
TrishaRetro <38111308+TrishaRetro@users.noreply.github.com>
2019-02-27 08:22:23 +0200

    Update clear.js

--- episode_13_code/clear.js
+++ episode_13_code/clear.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**[!]** Bu komutu kullanmak için yetkiniz bulunmamaktadır.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send(`${args[0]} adet mesaj silindi.`).then(msg => msg.delete(2000));
 });
 
 }
 
 module.exports.help = {
-  name: "clear"
+  name: "temizle"
 }
